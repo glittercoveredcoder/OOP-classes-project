d976020db9a87fbd9e5f3907e96bccd6f69ce44e
Alyssa DaCosta <alyssalrd@gmail.com>
2025-11-27 11:33:20 -0700

    finished logic for all patient methods

--- patients.py
+++ patients.py
@@ -24,3 +24,21 @@ class Patient:
         return self.age
     
     # Setters
+
+    def set_patient_id(self, new_id):
+        self.pid = new_id
+
+    def set_patient_name(self, new_name):
+        self.name = new_name
+    
+    def set_patient_disease(self, new_disease):
+        self.disease = new_disease
+
+    def set_patient_gender(self, new_gender):
+        self.gender = new_gender
+
+    def set_patient_age(self, new_age):
+        self.age = new_age
+
+    def __str__(self):
+        return f"{self.pid}_{self.name}_{self.disease}_{self.gender}_{self.age}"
